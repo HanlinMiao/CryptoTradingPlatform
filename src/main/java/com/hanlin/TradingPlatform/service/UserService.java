367c7122d4f51c28d2fc60963945915886b2c9e0
HanlinMiao <hanlin980505@gmail.com>
2025-01-19 22:59:51 -0500

    Implemented UserService

--- src/main/java/com/hanlin/TradingPlatform/service/UserService.java
+++ src/main/java/com/hanlin/TradingPlatform/service/UserService.java
@@ -1,12 +1,18 @@
 package com.hanlin.TradingPlatform.service;
 
+import com.hanlin.TradingPlatform.domain.VerificationType;
 import com.hanlin.TradingPlatform.model.User;
 
 public interface UserService {
 
-    public User findUserProfileByJwt(String jwt);
-    public User findUserByEmail(String email);
-    public User findUserById(Long userId);
-    public User enableTwoFactorAuthentication(User user);
+    public User findUserProfileByJwt(String jwt) throws Exception;
+    public User findUserByEmail(String email) throws Exception;
+    public User findUserById(Long userId) throws Exception;
+
+    public User enableTwoFactorAuthentication(
+            VerificationType verificationType,
+            String sendTo,
+            User user
+    );
     User updatePassword(User user, String newPassword);
 }
--- src/main/java/com/hanlin/TradingPlatform/service/UserServiceImpl.java
+++ src/main/java/com/hanlin/TradingPlatform/service/UserServiceImpl.java
@@ -2,34 +2,66 @@ package com.hanlin.TradingPlatform.service;
 
 
 import com.hanlin.TradingPlatform.config.JwtProvider;
+import com.hanlin.TradingPlatform.domain.VerificationType;
+import com.hanlin.TradingPlatform.model.TwoFactorAuth;
 import com.hanlin.TradingPlatform.model.User;
+import com.hanlin.TradingPlatform.repository.UserRepository;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class UserServiceImpl implements UserService{
 
-    @Override
-    public User findUserProfileByJwt(String jwt) {
-        return null;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
-    public User findUserByEmail(String email) {
-        return null;
+    public User findUserProfileByJwt(String jwt) throws Exception{
+        String email = JwtProvider.getEmailFromToken(jwt);
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new Exception("User not found");
+        }
+        return user;
     }
 
     @Override
-    public User findUserById(Long userId) {
-        return null;
+    public User findUserByEmail(String email) throws Exception {
+        User user = userRepository.findByEmail(email);
+
+        if (user == null) {
+            throw new Exception("User not found");
+        }
+        return user;
     }
 
     @Override
-    public User enableTwoFactorAuthentication(User user) {
-        return null;
+    public User findUserById(Long userId) throws Exception {
+        Optional<User> user = userRepository.findById(userId);
+        if(user.isEmpty()) {
+            throw new Exception("User not found");
+        }
+        return user.get();
+    }
+
+    @Override
+    public User enableTwoFactorAuthentication(VerificationType verificationType, String sendTo, User user) {
+        TwoFactorAuth twoFactorAuth = new TwoFactorAuth();
+        twoFactorAuth.setEnabled(true);
+        twoFactorAuth.setSendTo(verificationType);
+        user.setTwoFactorAuth(twoFactorAuth);
+
+        return userRepository.save(user);
     }
 
     @Override
     public User updatePassword(User user, String newPassword) {
-        return null;
+        user.setPassword(newPassword);
+        return userRepository.save(user);
     }
 }
